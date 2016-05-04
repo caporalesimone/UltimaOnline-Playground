df58b645c7f24b16867500bd57ccdb8ab0c64230
Caporale Simone <caporalesimone@users.noreply.github.com>
2016-05-04 07:44:21 +0200

    Udate Magery Script

--- Scripts/Magery/Magery.cpp
+++ Scripts/Magery/Magery.cpp
@@ -14,7 +14,38 @@ int main()
 	while (true)
 	{
 		printf("%d - Cast spell\n", cnt++);
-		uo.UseMacro(UOWrapper::Macro::Magery::Circle_7::Cast_Spell_Mana_Vampire);
+		int base, real, cap;
+		UOWrapper::Skills::LockStatus status;
+		uo.GetSkill(UOWrapper::Skills::Magery, base, real, cap, status);
+		
+		if (base < 400)
+		{
+			printf("You need at least 40.0 Magery\n");
+			exit(EXIT_FAILURE);
+		}
+		else if (base >= 400 && base <= 550)
+		{
+			uo.UseMacro(UOWrapper::Macro::Magery::Circle_4::Cast_Spell_Mana_Drain);
+		}
+		else if (base > 550 && base <= 650)
+		{
+			uo.UseMacro(UOWrapper::Macro::Magery::Circle_5::Cast_Spell_Paralyze);
+		}
+		else if (base > 650 && base <= 750)
+		{
+			uo.UseMacro(UOWrapper::Macro::Magery::Circle_6::Cast_Spell_Reveal);
+		}
+		else if (base > 750 && base <= 950)
+		{
+			uo.UseMacro(UOWrapper::Macro::Magery::Circle_7::Cast_Spell_Mana_Vampire);
+		}
+		else if (base > 950)
+		{
+			uo.UseMacro(UOWrapper::Macro::Magery::Circle_8::Cast_Spell_Earthquake);
+		}
+
+
+
 		::Sleep(150);
 		printf("Wait for target\n");
 		uo.WaitForTarget();
@@ -26,14 +57,14 @@ int main()
 		bool retval;
 		retval = uo.GetIntegerVar(UOWrapper::CharStatus::Mana, mana);
 		retval = uo.GetIntegerVar(UOWrapper::CharStatus::MaxMana, max_mana);
-		if (mana <= 35)
+		if (mana <= 20)
 		{
-			/*
 			printf("No Mana\n");
 			::Sleep(2000);
 			printf("Meditation\n");
 			uo.UseMacro(UOWrapper::Macro::UseSkill::Meditation);
 			::Sleep(10000);
+			/*
 			printf("Hiding\n");
 			uo.UseMacro(UOWrapper::Macro::UseSkill::Hiding);
 			::Sleep(10000);
--- Scripts/Taming/Taming.cpp
+++ Scripts/Taming/Taming.cpp
@@ -8,6 +8,118 @@ int main()
 {
 	UOWrapper uo;
 	uo.InitWrapper();
+	int tameable_list[] =
+	{
+		//0x7t,		// Hellcat (Large)
+		//0x62,		// Hellhound
+		//0x7a,		// Unicorn
+		//0x84,		// Ki-Rin
+		//0xbc,		// Savage Ridgeback
+		//0xbb,		// Ridgeback
+		//0x17,		// Dire Wolf
+		//0xce,		// Lava Lizard
+		//0x14,		// Frost spider
+		//0xc9,		// Hellcat (Small)
+		//0xda,		// Frenzied Ostard
+		//0x50,		// Giant Toad
+		0xe8,		// Bull
+		0xe9,		// Bull2
+					//0x22,		// White Wolf
+					//0x25,		// White Wolf2
+					//0x1c,		// Giant Spider
+					//0xd4,		// Grizzly Bear
+					//0xea,		// Great Hart
+					//0x41,		// Snow leopard
+					//0x40,		// Snow leopard
+					//0x19,		// Gray wolf
+					//0x1b,		// Gray wolf2
+					//0x30,		// Scorpion
+					//0xca,		// Alligator
+					//0x3f,		// Cougar
+					//0xa7,		// Brown Bear
+					//0xdd,		// Walrus
+					//0xd5,		// Polar Bear
+					//0xd3,		// Black Bear
+					//0xdc,		// Llama
+					//0x124,	// Pack Llama
+					//0x123,	// Pack Horse
+					//0xc8,		// Horse
+					//0xe2,		// Horse2
+					//0xcc,		// Horse3
+					//0xed,		// Giant Rat
+					//0xdb,		// Forest Ostard
+					//0xd2,		// Desert Ostard
+					//0x122,	// Boar
+					//0xe1,		// Timber Wolf
+					//0xed,		// Hind
+					//0x51,		// Bullfrog
+	};
+
+
+
+	UOWrapper uo;
+	uo.InitWrapper();
+
+	uo.UseMacro(UOWrapper::Macro::GumpControl::Open::Status);
+	::Sleep(1000);
+
+
+
+	int tameable_list_size = sizeof(tameable_list) / sizeof(tameable_list[0]);
+
+	while (true)
+	{
+		int itemcount;
+		REUONetCommandScanItems(true, &itemcount);
+
+		struct Item
+		{
+			int ID;
+			int Type;
+			int Kind;
+			int PosX;
+			int PosY;
+			int PosZ;
+			int ContID;
+			int Stack;
+			int Reputation;
+			int Color;
+		};
+
+		int index = 0xe8;
+		int x, y, z, id, type, kind, contId, stack, rep, col;
+		for (int index = 0; index < itemcount; index++)
+		{
+			REUONetCommandGetItem(index, &id, &type, &kind, &contId, &x, &y, &z, &stack, &rep, &col);
+			Item iTemp;
+			iTemp.ID = id;
+			iTemp.Type = type;
+			iTemp.Kind = kind;
+			iTemp.PosX = x;
+			iTemp.PosY = y;
+			iTemp.PosZ = z;
+			iTemp.ContID = contId;
+			iTemp.Stack = stack;
+			iTemp.Reputation = rep;
+			iTemp.Color = col;
+
+			if (type == 233) {
+				char Name[100];
+				char Info[1000];
+				REUONetCommandProperty(id, Name, 100, Info, 1000);
+				printf("%s\tx=%d - y=%d - z=%d\n", Name,x,y,z);
+				//printf("%s\n--------------\n", Info);
+				id = id;
+			}
+		}
+
+		char line[100];
+		int color;
+		int ref; int count;
+		REUONetCommandScanJournal(0, &ref, &count);
+		REUONetCommandGetJournal(0, line, 100, &color);
+
+	}
 
     return 0;
 }
