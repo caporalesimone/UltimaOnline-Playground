0c03093b1e2f36fec42060f94688024b9babbc97
Caporale Simone <caporalesimone@users.noreply.github.com>
2016-05-07 18:15:17 +0200

    Tolto il target self nelle spell che non hanno target

--- Scripts/Magery/Magery.cpp
+++ Scripts/Magery/Magery.cpp
@@ -13,11 +13,12 @@ int main()
 	int cnt = 1;
 	while (true)
 	{
+		bool target_self = true;
 		printf("%d - Casting spell ", cnt++);
 		int base, real, cap;
 		UOWrapper::Skills::LockStatus status;
 		uo.GetSkill(UOWrapper::Skills::Magery, base, real, cap, status);
-		
+
 		if (base < 400)
 		{
 			printf("You need at least 40.0 Magery\n");
@@ -38,47 +39,47 @@ int main()
 			uo.UseMacro(UOWrapper::Macro::Magery::Circle_6::Cast_Spell_Reveal);
 			printf("Reveal\n");
 		}
-		else if (base > 750 && base <= 950)
+		else if (base > 750 && base <= 919)
 		{
 			uo.UseMacro(UOWrapper::Macro::Magery::Circle_7::Cast_Spell_Mana_Vampire);
 			printf("Mana Vampire\n");
 		}
-		else if (base > 950)
+		else if (base > 919)
 		{
 			uo.UseMacro(UOWrapper::Macro::Magery::Circle_8::Cast_Spell_Earthquake);
 			printf("Earthquake\n");
 			//TODO: Non richiede il target mi pare quindi usare un bool per skippare la wait for target
+			target_self = false;
 		}
 
 		::Sleep(150);
-		printf("Wait for target\n");
-		uo.WaitForTarget();
-		printf("Target Self\n");
-		::Sleep(200);
-		uo.UseMacro(UOWrapper::Macro::Miscellaneous::Target_Self);
-		::Sleep(1000);
+
+		if (target_self == true)
+		{
+			printf("Wait for target\n");
+			uo.WaitForTarget();
+			printf("Target Self\n");
+			::Sleep(200);
+			uo.UseMacro(UOWrapper::Macro::Miscellaneous::Target_Self);
+			::Sleep(1000);
+		}
+
 		int mana, max_mana;
 		bool retval;
 		retval = uo.GetIntegerVar(UOWrapper::CharStatus::Mana, mana);
 		retval = uo.GetIntegerVar(UOWrapper::CharStatus::MaxMana, max_mana);
-		if (mana <= 20)
+		if (mana <= 60)
 		{
 			printf("No Mana\n");
 			::Sleep(2000);
-			printf("Meditation\n");
-			uo.UseMacro(UOWrapper::Macro::UseSkill::Meditation);
-			::Sleep(10000);
-			/*
-			printf("Hiding\n");
-			uo.UseMacro(UOWrapper::Macro::UseSkill::Hiding);
-			::Sleep(10000);
-			*/
-			printf("Wait for Mana\n");
 			while (mana < max_mana)
 			{
+				printf("Meditation\n");
+				uo.UseMacro(UOWrapper::Macro::UseSkill::Meditation);
+				::Sleep(10000);
 				retval = uo.GetIntegerVar(UOWrapper::CharStatus::Mana, mana);
 				printf("\t Mana: %d/%d\n", mana, max_mana);
-				::Sleep(1000);
+				//::Sleep(1000);
 			}
 		}
 		::Sleep(2500);
